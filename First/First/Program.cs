e76686750403a384e1286c6571a87ded6e8e00b6
Valentyn <valikharchenko1@gmail.com>
2023-09-18 20:11:37 +0300

    second commit

--- First/First/Program.cs
+++ First/First/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    public static bool Hola { get; private set; }
+
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(Hola);
+    }
+}
